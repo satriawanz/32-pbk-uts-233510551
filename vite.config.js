6459be6168fbdf73503bea418fa0834a6a3df19e
satriawanz <rahmatsatriawan04@gmail.com>
2025-05-08 19:23:10 +0700

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/32-pbk-uts-233510551/',
   plugins: [vue(),
     tailwindcss()
   ],
