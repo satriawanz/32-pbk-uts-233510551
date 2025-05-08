d528abe55748f3e3072c39e91a8c8a57341d318e
satriawanz <rahmatsatriawan04@gmail.com>
2025-05-08 18:10:46 +0700

    input list

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const list = ref([]);
+const newList = ref('');
+
+const addList = () => {
+  if (newList.value !== '') {
+    list.value.push({
+      id: list.value.length + 1,
+      name: newList.value,
+      isDone: false,
+    });
+    newList.value = '';
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input type="text" v-model="newList" />
+    <button @click="addList">Add</button>
+  </div>
+</template>
+
+<style scoped></style>
