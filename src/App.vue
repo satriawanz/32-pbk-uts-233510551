65c8a46b124a55c96ef0a9c967cfbfb0ac49a651
satriawanz <rahmatsatriawan04@gmail.com>
2025-05-08 18:37:57 +0700

    filter list

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const list = ref([]);
 const newList = ref('');
+const filter = ref('all');
 
 const addList = () => {
   if (newList.value !== '') {
@@ -19,15 +20,30 @@ const removeList = (id) => {
   list.value = list.value.filter((item) => item.id !== id);
 }
 
+const filterList = computed(() => {
+  if (filter.value === 'all') {
+    return list.value;
+  } else if (filter.value === 'done') {
+    return list.value.filter((item) => item.isDone);
+  } else {
+    return list.value.filter((item) => !item.isDone);
+  }
+})
+
 </script>
 
 <template>
   <div>
     <input type="text" v-model="newList" />
     <button @click="addList">Add</button>
+    <select v-model="filter">
+      <option value="all">All</option>
+      <option value="done">Done</option>
+      <option value="not-done">Not Done</option>
+    </select>
 
     <ul>
-      <li v-for="item in list" :key="item.id">
+      <li v-for="item in filterList" :key="item.id">
         <input type="checkbox" v-model="item.isDone" />
         <span :class="item.isDone ? 'line-through text-gray-400' : ''">
           {{ item.name }}
